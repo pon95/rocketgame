a9e3776d5300ed243272bae3976dcdcc0c4692d5
Fander Company <faddishfaun4807life@gmail.com>
2024-03-03 17:33:53 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import os
+
+def run_game():
+    os.system('python game.py')
+
+def run_server_game():
+    os.system('start cmd /k python server.py & python game.py')
+
+# Create the main window
+root = tk.Tk()
+root.title("Игра")
+
+# Button for "Я игрок"
+player_button = tk.Button(root, text="Я игрок", command=run_game)
+player_button.pack()
+
+# Button for "Я сервер"
+server_button = tk.Button(root, text="Я сервер", command=run_server_game)
+server_button.pack()
+
+# Start the Tkinter main loop
+root.mainloop()
